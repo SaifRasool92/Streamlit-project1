880ee2eb117f7a758fcc7b5b57e3c58c72906978
SaifRasool92 <rasoolsaif24@gmail.com>
2024-09-05 20:55:05 +0500

    updated code

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+# st.write("This is a simple Streamlit app.")
+
+number = st.slider("Pick a number", 0, 100)
+st.write(f"The square of {number} is {number ** 2}.")  # Corrected line
+
